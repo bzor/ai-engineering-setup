b09c3c76eb5dcd320dce48e9d2252ef4416083a1
bzor <kc@bzor.com>
2026-07-19 15:20:11 -0400

    nvim: center window after {/} block jumps

--- configs/nvim/lua/bzor/remap.lua
+++ configs/nvim/lua/bzor/remap.lua
@@ -1,3 +1,7 @@
 -- Center the window after half-page scrolls
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down half page and center" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half page and center" })
+
+-- Center the window after paragraph/block jumps
+vim.keymap.set("n", "{", "{zz", { desc = "Jump to previous block and center" })
+vim.keymap.set("n", "}", "}zz", { desc = "Jump to next block and center" })
